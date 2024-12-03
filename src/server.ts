8169015e9d5a5693183660896fa880458ed15264
tanmoyach99e <tanmoyacharjee18@gmail.comm>
2024-12-03 11:15:46 +0600

    linting and setup done

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
 
+
+
+const port= config.port||3000
 async function main() {
   try {
     await mongoose.connect(config.db_url as string);
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+    // app.listen(port, () => {
+    //   console.log(`Example app listening on port ${port}`);
+    // });
+    app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
   } catch (err) {
     console.log(err);
   }
